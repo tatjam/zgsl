2ca5918830d134961c6c2c2044d05ca0dd337703
tatjam <tatjam02@gmail.com>
2024-07-30 15:12:13 +0200

    Initial ideas

---
+++ README.md
@@ -0,0 +1,5 @@
+# LICENSE
+
+Note that the Zig wrapper around GSL is licensed under MIT, but GSL itself is under GPL, 
+and thus usage of this library implies your software must be licensed under GPL 
+(as long as it's distributed).
--- build.zig
+++ build.zig
@@ -1,36 +1,50 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
+
+    const upstream = b.dependency("gsl", .{});
+
+    const config_gen_tool = b.addExecutable(.{
+        .name = "config_gen",
+        .root_source_file = b.path("src/config_gen.zig"),
+        .target = b.host
+    });
+
+    const config_gen_step = b.addRunArtifact(config_gen_tool);
+    const config_file = config_gen_step.addOutputFileArg("config.h");
+    _ = config_gen_step.addOutputDirectoryArg("cfiles");
+
+    const gls_lib = b.addStaticLibrary(.{
+        .name = "gsl",
+        .optimize = optimize,
+        .target = target
+    });
+    gls_lib.linkLibC();
+
+
+    // All source files in gls
+    gls_lib.addCSourceFiles(.{
+        .root = upstream.path(""),
+        .files = &.{
+            "blas/blas.c"
+        }
+    });
+    gls_lib.addIncludePath(upstream.path(""));
+    gls_lib.addIncludePath(config_file.dirname());
 
     const lib = b.addStaticLibrary(.{
         .name = "zgsl",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
+    lib.linkLibrary(gls_lib);
+
     b.installArtifact(lib);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
     const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
@@ -39,12 +53,10 @@ pub fn build(b: *std.Build) void {
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
 
+    // For ZLS quick error reporting
     const lib_check = b.addTest(.{
         .name = "check",
         .root_source_file = b.path("src/root.zig"),
